a2f69990cdd56d69bef427d7318899a00b687134
Kabhatt <kb336@njit.edu>
2021-11-10 03:00:57 -0500

    added test file

--- test/calculator.py
+++ test/calculator.py
@@ -1,18 +1,11 @@
 def addition(x, y):
     return x + y
-
-
 def substraction(x, y):
     return x - y
-
-
 def multiply(x, y):
     return x * y
-
-
 def divide(x, y):
     return x / y
-
 
 print("1.ADD")
 print("2.SUBTRACT")
@@ -21,7 +14,6 @@ print("4.DIVIDE")
 
 while True:
     choice = input("Please Choose an operation to preform ( 1/2/3/4: )")
-
     if choice in ("1", "2", "3", "4"):
         num1 = float(input("Enter First Number: "))
         num2 = float(input("Enter Second Number: "))
